67b2f5eac5c910134d7c71ebca74ca64e71a158c
koigor97 <turaysamuel101@yahoo.com>
2025-10-10 12:14:35 +0200

    add how it works section, divider component,  and feature section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,13 +1,16 @@
+import FeatureSection from "@/components/public-pages/landing-page/featureSection";
 import HomeHeroSection from "@/components/public-pages/landing-page/heroSection";
-import WhatWeDoSection from "@/components/public-pages/landing-page/whatWeDoSection";
-
+import HowItWorks from "@/components/public-pages/landing-page/howItWorksSection";
+import Divider from "@/components/ui/divider";
 
 
 export default function Home() {
   return (
-    <div className="grid gap-10 items-center justify-center">
+    <div className="grid gap-5 items-center justify-center">
         <HomeHeroSection />
-        <WhatWeDoSection />
+        <Divider className="my-8" width={"md:w-3xl md:mx-auto"}  variant="dashed"/>
+        <HowItWorks />
+        <FeatureSection />
     </div>
   );
 }
