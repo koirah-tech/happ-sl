639ea467392ff4083b4407cb21ef8417477b04ec
koigor97 <turaysamuel101@yahoo.com>
2025-10-11 20:55:53 +0200

    modify UI/UX

--- components/cool/dot-card.tsx
+++ components/cool/dot-card.tsx
@@ -5,14 +5,15 @@ export interface DotCardProps {
   title: string;
   description: string;
   bgStyling: string;
+  step?: number;
 }
 
-export default function DotCard({icon: Icon, title, description, bgStyling}: DotCardProps) {
+export default function DotCard({icon: Icon, title, description, bgStyling, step}: DotCardProps) {
   return (
     <div className="relative mx-auto w-full max-w-sm px-4  sm:px-6 md:px-8">
       <div className="absolute left-0 top-4 -z-0 h-px w-full bg-zinc-400 dark:bg-zinc-700 sm:top-6 md:top-8" />
       <div className="absolute bottom-4 left-0 z-0 h-px w-full bg-zinc-400 dark:bg-zinc-700 sm:bottom-6 md:bottom-8" />
-      <div className="relative w-full border-x border-zinc-400 dark:border-zinc-700">
+      <div className="relative w-full border-x border-zinc-400 dark:border-zinc-700 step-badge" data-step={step}>
         <div className="absolute z-0 grid h-full w-full items-center">
           <section className="absolute z-0 grid h-full w-full grid-cols-2 place-content-between">
             <div className="my-4 size-1 -translate-x-[2.5px] rounded-full bg-primary  outline-8 outline-gray-50 dark:outline-gray-950 sm:my-6 md:my-8" />
@@ -23,7 +24,9 @@ export default function DotCard({icon: Icon, title, description, bgStyling}: Dot
         </div>
         <div className="relative z-20 mx-auto py-8">
           <div className="p-6">
-            <div className={`${bgStyling} w-fit p-2 mb-5`}>
+            <div 
+              className={`${bgStyling} relative w-fit p-2 mb-5`}
+              >
               <Icon width={65} height={65} />
             </div>
             <h3 className="mb-1 text-lg font-bold text-gray-900 dark:text-gray-100">
