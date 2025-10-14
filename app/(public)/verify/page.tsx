bec5d2496812d456fb8c253e0d4fdd24080a004d
koigor97 <turaysamuel101@yahoo.com>
2025-10-14 19:32:35 +0200

    create verify page

---
+++ app/(public)/verify/page.tsx
@@ -0,0 +1,12 @@
+
+import VerificationHub from "@/components/public-pages/verification-page/verificationHub";
+
+export default function VerifyPage() {
+  return (
+    <main className="relative flex min-h-[70vh] items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="relative z-10 w-full">
+        <VerificationHub />
+      </div>
+    </main>
+  );
+}
